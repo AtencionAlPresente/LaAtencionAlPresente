d3e75eb46d63e967bc720391408d1725542f85b7
darkchop <ecs200@hotmail.com>
2017-07-14 10:47:54 +0200

    Creacion layouts

--- app/src/main/java/com/laatencionalpresente/www/laatencionalpresente/MainActivity.java
+++ app/src/main/java/com/laatencionalpresente/www/laatencionalpresente/MainActivity.java
@@ -1,5 +1,6 @@
 package com.laatencionalpresente.www.laatencionalpresente;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola
+    
+        Intent i = new Intent(this,ActivityPrincipal.class);
+        startActivity(i);
     }
 }
