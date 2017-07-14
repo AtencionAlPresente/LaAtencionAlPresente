16e4841e6566940e3838961eecceb6bf6cbf98ea
darkchop <ecs200@hotmail.com>
2017-07-14 10:40:03 +0200

    init

---
+++ app/src/main/java/com/laatencionalpresente/www/laatencionalpresente/MainActivity.java
@@ -0,0 +1,13 @@
+package com.laatencionalpresente.www.laatencionalpresente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
